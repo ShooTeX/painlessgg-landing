0a9c91b4d15cf53963ea71fd707c729b771a854b
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2023-01-02 01:56:46 +0100

    initial design

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,22 @@
-/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import("tailwindcss").Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      "black": "#000000",
+      "white": "#FFFFFF",
+      "goldenrod-yellow": "#FCCB90",
+      "calypso-orchid": "#D57EEB",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
