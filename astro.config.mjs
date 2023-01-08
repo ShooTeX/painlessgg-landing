6974ca3f5e1e911e76ea3c7483b7ee26db74733b
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2023-01-08 15:01:34 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://painless.gg",
+  site: "https://www.painless.gg",
   integrations: [tailwind(), sitemap()]
 });
